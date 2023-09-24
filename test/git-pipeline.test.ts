c9a65d34dd511f07b4ce6ca71c161c52b87abb7c
Shadhin Lab <shadhinlab@MacBook-Pro-2.local>
2023-09-24 10:25:41 +0600

    Initial commit

---
+++ test/git-pipeline.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as GitPipeline from '../lib/git-pipeline-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/git-pipeline-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new GitPipeline.GitPipelineStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
