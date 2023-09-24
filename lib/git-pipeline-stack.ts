6ec23bfcc45c11d0ec64a8cda5afe6cbba393852
Abdullah Al Hasib <a.hasib.rifat@gmail.com>
2023-09-24 13:14:43 +0600

    feat: codebuild

--- lib/git-pipeline-stack.ts
+++ lib/git-pipeline-stack.ts
@@ -1,4 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
+import { LinuxBuildImage, BuildSpec } from 'aws-cdk-lib/aws-codebuild';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
 
@@ -8,6 +9,21 @@ export class GitPipelineStack extends cdk.Stack {
 
     new CodePipeline(this, "AwesomePipeline", {
       pipelineName: "AwesomePipeline",
+      codeBuildDefaults: {
+        buildEnvironment: {
+          buildImage: LinuxBuildImage.AMAZON_LINUX_2_5,
+        },
+        partialBuildSpec: BuildSpec.fromObject({
+          version: 0.2,
+          phase: {
+            install : {
+              'runtime-version': {
+                nodejs: "latest"
+              }
+            }
+          }
+        })
+      },
       synth: new ShellStep("Synth", {
         input: CodePipelineSource.gitHub("rifat17/aws-cicd-pipeline", "main", {
           authentication: cdk.SecretValue.secretsManager('github-token')
